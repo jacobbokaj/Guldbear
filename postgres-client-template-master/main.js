c88442b9c07700d4e477b949edc154744beaf6b5
Jacob <jacobvestergaard@hotmail.dk>
2021-12-06 14:51:40 +0100

    f

--- postgres-client-template-master/main.js
+++ postgres-client-template-master/main.js
@@ -18,7 +18,7 @@ const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_NAME = process.env.DB_NAME || "kristians-database";
 const DB_PW = process.env.DB_PW || "password";
 const DB_PORT = process.env.DB_PORT || 5432;
-
+console.log(DB_NAME);
 /**
  * I stedet for at ændre på DB-værdierne i koden herover, er det bedre at gøre det som
  * en del af den måde man kører programmet på. Hver DB-værdi kan sættes i terminalen
