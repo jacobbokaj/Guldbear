24366d51eabf478052a48656e5e31c2a8d0e0c16
jacob <jacobvestergaard@hotmail.dk>
2021-12-08 10:25:56 +0100

    gi

--- postgres-client-template-master/main.js
+++ postgres-client-template-master/main.js
@@ -48,10 +48,10 @@ if (!process.env.DB_NAME || !process.env.DB_PW || !process.env.DB_USER) {
  */
 const app = express();
 const client = new Client({
-  user: DB_USER,
-  host: DB_HOST,
-  database: DB_NAME,
-  password: DB_PW,
+  user: "lpqshzta",
+  host: "ella.db.elephantsql.com",
+  database: "lpqshzta",
+  password: "h9Oq5ly0OVUd6igyiwpdKPSytlZTh6Xd",
   port: 5432
 });
 client.connect();
